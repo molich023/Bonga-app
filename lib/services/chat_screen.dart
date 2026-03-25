de290f65bbb010c714ab52a7147ac9649f41c333
Molich <molich60@gmail.com>
2026-03-26 00:26:51 +0300

    Delete lib/services/chat_screen.dart

--- lib/services/chat_screen.dart
+++
@@ -1,7 +0,0 @@
-// Add to _ChatScreenState
-Future<void> _inviteFriend() async {
-  final response = await ApiService.mintReward(widget.user, 'invite');
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(content: Text('Earned ${response['points']} Bonga Points!')),
-  );
-}
