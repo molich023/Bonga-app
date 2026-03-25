25b3e90ce82dd86a2c9707717f6a7af85b4190a1
Molich <molich60@gmail.com>
2026-03-26 00:59:30 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
+import 'package:bonga_gropesa/screens/chat_screen.dart';
+import 'package:bonga_gropesa/screens/rewards_screen.dart';
+import 'package:bonga_gropesa/screens/profile_screen.dart';
 
 void main() {
   runApp(const BongaApp());
@@ -11,11 +14,14 @@ class BongaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bonga App',
-      theme: ThemeData(
-        primarySwatch: Colors.green, // Kenyan-inspired color
-      ),
-      home: const LoginScreen(),
+      title: 'Bonga + GroPesa',
+      theme: ThemeData(primarySwatch: Colors.green),
+      initialRoute: '/chat',
+      routes: {
+        '/chat': (context) => const ChatScreen(),
+        '/rewards': (context) => const RewardsScreen(),
+        '/profile': (context) => const ProfileScreen(),
+      },
     );
   }
 }
